dcab96138fcf32ae2d09d0022ecfe4a6e5d64de9
Marwan Wael El Zaher <m.wael@penta-b.com>
2024-03-13 00:58:14 +0200

    fix: fix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,5 +2,6 @@
 def uppercase(str):
     for char in str:
         if ord(char) >= ord("a") and ord(char) <= ord("z"):
-            print("{:c}".format(ord(char) - 32), end=(""))
+            uppercase_char = chr(ord(char) - 32)
+            print("{}".format(uppercase_char), end=(""))
     print("")

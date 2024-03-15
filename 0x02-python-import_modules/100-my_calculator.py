05d1d5c7967cc6bc4d17ae46397a1c9336b18bae
MarwanElZaher <marawanmzaher@gmail.com>
2024-03-15 02:21:46 +0200

    advanced calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    from sys import argv
+    from sys import argv, exit
     if(len(argv) != 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)

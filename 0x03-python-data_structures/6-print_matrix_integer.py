cb5728cd925303af79c8bda00f2978be7de452e4
Marwan <marawanmzaher@hotmail.com>
2024-03-17 23:08:12 +0200

    corrected the name of the file

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in range(len(matrix)):
-        for column in range(len(matrix[row])):
-            print("{:d}".format(matrix[row][column]), end="")
-            if column != (len(matrix[row]) - 1):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
                 print(" ", end="")
-            if (column + 1) % 3 == 0:
-                print()
+        print("")

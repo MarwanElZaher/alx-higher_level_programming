4c684150ed1f1917395227d89a547011c43801e9
Marwan <marawanmzaher@hotmail.com>
2024-03-18 00:11:42 +0200

    divisible by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    list_mult = []
+    for num in range(len(my_list)):
+        if my_list[num] % 2 == 0:
+            list_mult.append(True)
+        else:
+            list_mult.append(False)
+    return(list_mult)
